38075bd01585f89137b97191a48c08fe64a2296f
hschatzmann <117648267+hschatzmann@users.noreply.github.com>
2024-05-27 11:23:51 +0200

    this might actually work now

--- eth_mugs_dataset.py
+++ eth_mugs_dataset.py
@@ -1,16 +1,11 @@
-"""ETH Mugs Dataset."""
-
 import os
 from PIL import Image
 import torch
-
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-from utils import IMAGE_SIZE, load_mask
+from utils import IMAGE_SIZE, load_mask  # Ensure this import is correct
 
-
-# This is only an example - you DO NOT have to use it exactly in this form!
 class ETHMugsDataset(Dataset):
     """Torch dataset template shared as an example."""
 
@@ -27,18 +22,19 @@ class ETHMugsDataset(Dataset):
         self.mode = mode
         self.root_dir = root_dir
 
-        # TODO: get image and mask paths
+        # Get image and mask paths
         self.rgb_dir = os.path.join(self.root_dir, "rgb")
         self.mask_dir = os.path.join(self.root_dir, "masks")
-        self.image_paths = os.path.join(self.root_dir, "rgb")  #TODO: what the fuck is this for? only len check? --> just use rgb again I guess
 
-        # TODO: set image transforms - these transforms will be applied to pre-process the data before passing it through the model
-        self.transform = transforms.ToTensor()  # TODO just to tensor rn, possibly normalize for better results later
+        self.image_paths = sorted([os.path.join(self.rgb_dir, f) for f in os.listdir(self.rgb_dir) if f.endswith('.png') or f.endswith('.jpg')])
+        if self.mode != 'test':
+            self.mask_paths = sorted([os.path.join(self.mask_dir, f) for f in os.listdir(self.mask_dir) if f.endswith('.png') or f.endswith('.jpg')])
+
+        # Set image transforms
+        self.transform = transforms.ToTensor()  # You can add more transformations as needed
 
         print("[INFO] Dataset mode:", mode)
-        print(
-            "[INFO] Number of images in the ETHMugDataset: {}".format(len(self.image_paths))
-        )
+        print("[INFO] Number of images in the ETHMugDataset: {}".format(len(self.image_paths)))
 
     def __len__(self):
         """Return the length of the dataset."""
@@ -46,13 +42,20 @@ class ETHMugsDataset(Dataset):
 
     def __getitem__(self, idx: int):
         """Get an item from the dataset."""
-        # TODO: load image and gt mask (unless when in test mode), apply transforms if necessary
-        # TODO this means getting it, transforming it to tensor i guess?
+        # Load image
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert('RGB')  # Ensure image is in RGB format
+        image = self.transform(image)
 
-        image = Image.open(self.rgb_dir + idx + "_rgb")     #open image, no conversion because should be in right format
-        image = self.transform(image)                       #transform image
+        if self.mode != 'test':
+            # Load mask
+            mask_path = self.mask_paths[idx]
+            mask = Image.open(mask_path).convert('L')  # Assuming mask is in grayscale
+            mask = self.transform(mask)
+            return image, mask
+        else:
+            return image
 
-        mask = Image.open(self.mask_dir + idx +"_mask")     #open mask, no conversion because should be in right format
-        mask = self.transform(mask)                         #transfrom mask
-
-        return image, mask
+# Usage example:
+# dataset = ETHMugsDataset(root_dir='./datasets/train_images_378_252', mode='train')
+# image, mask = dataset[0]
